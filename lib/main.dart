3050cbef8564dc0e2b7cbd82a036828b0b42ddcd
andre.lubambi <andrelubambi36@gmail.com>
2025-03-20 11:00:04 +0100

    feat(theme): change theme app with ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:gerenciamento_de_estado/builders/stream_notifier_builder.dart';
 import 'package:gerenciamento_de_estado/controllers/stream_notifier_imp.dart';
 import 'package:flutter/material.dart';
+import 'package:gerenciamento_de_estado/presention/controllers/theme_controller.dart';
 
 void main() => runApp(const MyApp());
+
+final themeController = ThemeController();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: false),
-      home: const MyHomePage(),
+    return ListenableBuilder(
+      listenable: themeController,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme:
+              themeController.isDarkTheme
+                  ? ThemeData.dark()
+                  : ThemeData.light(),
+          home: const MyHomePage(),
+        );
+      },
     );
   }
 }
@@ -27,35 +38,40 @@ class _MyHomePageState extends State<MyHomePage> {
   final _counterNotifier = StreamNotifier(0);
 
   @override
+  void initState() {
+    themeController.addListener(callback);
+    super.initState();
+  }
+
+  void callback() {
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     Theme.of(context);
     return Scaffold(
-      appBar: AppBar(title: const Text("Testando Streams")),
+      appBar: AppBar(title: const Text("ChangeNotifier")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            StreamNotifierBuilder(
-              listen: (context, state) {
-                ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Valor emitido: $state")),
-                );
+            const Text("Trocar tema do aplicativo"),
+            Switch(
+              value: themeController.isDarkTheme,
+              onChanged: (value) {
+                themeController.toggleTheme();
               },
-              streamNotifier: _counterNotifier,
-              builder: (context, state) {
-                return Text("Valor do counter: ${_counterNotifier.state}");
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _counterNotifier.emit(_counterNotifier.state + 1);
-              },
-              child: const Text("Incrementar"),
             ),
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    themeController.removeListener(callback);
+    super.dispose();
   }
 }
