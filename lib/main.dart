79fcba4d1cc81a520609b45c9408c834077286f0
andre.lubambi <andrelubambi36@gmail.com>
2025-03-20 12:41:42 +0100

    feat(State management): Implemented reactive state management with Streams

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,61 @@
 import 'package:gerenciamento_de_estado/builders/stream_notifier_builder.dart';
 import 'package:gerenciamento_de_estado/controllers/stream_notifier_imp.dart';
 import 'package:flutter/material.dart';
-import 'package:gerenciamento_de_estado/presention/controllers/theme_controller.dart';
-import 'package:gerenciamento_de_estado/presention/pages/my_home_page.dart';
 
 void main() => runApp(const MyApp());
-
-final themeController = ThemeController();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return ListenableBuilder(
-      listenable: themeController,
-      builder: (context, child) {
-        return MaterialApp(
-          title: 'Flutter Demo',
-          theme: themeController.value ? ThemeData.dark() : ThemeData.light(),
-          home: const MyHomePage(),
-        );
-      },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: false),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _counterNotifier = StreamNotifier(0);
+
+  @override
+  Widget build(BuildContext context) {
+    Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(title: const Text("Testando Streams")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamNotifierBuilder(
+              listen: (context, state) {
+                ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Valor emitido: $state")),
+                );
+              },
+              streamNotifier: _counterNotifier,
+              builder: (context, state) {
+                return Text("Valor do counter: ${_counterNotifier.state}");
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _counterNotifier.emit(_counterNotifier.state + 1);
+              },
+              child: const Text("Incrementar"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
